1dfdc7367d03467f425cca2d2864dc722cfbcb01
Luigi Iaco <iacominoluigi54@gmail.com>
2026-02-04 00:18:26 +0100

    .

--- siteReact/frontend/src/service/url.js
+++ siteReact/frontend/src/service/url.js
@@ -1,6 +1,6 @@
 //Url globali
 const SERVER_URL = "https://api.spoonacular.com";
-const BACKEND_URL = "http://localhost:3000";
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 //recipesUrl
 const recipesUrl = "/recipes/random";
